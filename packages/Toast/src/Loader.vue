215c93adc12382dd8bdee0aad1e686132f70b301
butingting <butingting@weidian.com>
2016-12-30 11:50:47 +0800

    优化spin动画代码

--- packages/Toast/src/Loader.vue
+++ packages/Toast/src/Loader.vue
@@ -15,7 +15,7 @@ export default {
 }
 </script>
 
-<style lang="css">
+<style lang="sass">
 .wd-spin-wrap {
   position: relative;
 }
@@ -31,77 +31,11 @@ export default {
   vertical-align: middle;
   margin-right: -9px;
 }
-.wd-spin-wrap > div:nth-child(1) {
-  -webkit-animation: wd-spin-wrap 1.2s -0.84s infinite ease-in-out;
-          animation: wd-spin-wrap 1.2s -0.84s infinite ease-in-out;
-    }
-.wd-spin-wrap > div:nth-child(2) {
-  -webkit-transform: rotate(30deg);
-          transform: rotate(30deg);
-          -webkit-animation: wd-spin-wrap 1.2s -0.72s infinite ease-in-out;
-                  animation: wd-spin-wrap 1.2s -0.72s infinite ease-in-out;
-}
-.wd-spin-wrap > div:nth-child(3) {
-  -webkit-transform: rotate(60deg);
-          transform: rotate(60deg);
-          -webkit-animation: wd-spin-wrap 1.2s -0.6s infinite ease-in-out;
-                  animation: wd-spin-wrap 1.2s -0.6s infinite ease-in-out;
+@for $i from 1 through 12 {
+  .wd-spin-wrap > div:nth-child(#{$i}) {
+    transform: rotate((30deg *($i - 1)));
+    animation: wd-spin-wrap 1.2s (-0.1*$i) * 1s infinite ease-in-out;
   }
-.wd-spin-wrap > div:nth-child(4) {
-  -webkit-transform: rotate(90deg);
-          transform: rotate(90deg);
-          -webkit-animation: wd-spin-wrap 1.2s -0.48s infinite ease-in-out;
-                  animation: wd-spin-wrap 1.2s -0.48s infinite ease-in-out;
-
-}
-.wd-spin-wrap > div:nth-child(5) {
-  -webkit-animation: wd-spin-wrap 1.2s -0.36s infinite ease-in-out;
-          animation: wd-spin-wrap 1.2s -0.36s infinite ease-in-out;
-  -webkit-transform: rotate(120deg);
-          transform: rotate(120deg);
-}
-.wd-spin-wrap > div:nth-child(6) {
-  -webkit-animation: wd-spin-wrap 1.2s -0.25s infinite ease-in-out;
-          animation: wd-spin-wrap 1.2s -0.25s infinite ease-in-out;
-  -webkit-transform: rotate(150deg);
-          transform: rotate(150deg);
-}
-.wd-spin-wrap > div:nth-child(7) {
-  -webkit-animation: wd-spin-wrap 1.2s -0.12s infinite ease-in-out;
-          animation: wd-spin-wrap 1.2s -0.12s infinite ease-in-out;
-  -webkit-transform: rotate(180deg);
-          transform: rotate(180deg);
-}
-.wd-spin-wrap > div:nth-child(8) {
-  -webkit-animation: wd-spin-wrap 1.2s -0s infinite ease-in-out;
-          animation: wd-spin-wrap 1.2s -0s infinite ease-in-out;
-  -webkit-transform: rotate(-150deg);
-          transform: rotate(-150deg);
-
-}
-.wd-spin-wrap > div:nth-child(9) {
-  -webkit-animation: wd-spin-wrap 1.2s -0s infinite ease-in-out;
-          animation: wd-spin-wrap 1.2s -0s infinite ease-in-out;
-  -webkit-transform: rotate(-120deg);
-          transform: rotate(-120deg);
-}
-.wd-spin-wrap > div:nth-child(10) {
-  -webkit-animation: wd-spin-wrap 1.2s -0s infinite ease-in-out;
-          animation: wd-spin-wrap 1.2s -0s infinite ease-in-out;
-  -webkit-transform: rotate(-90deg);
-          transform: rotate(-90deg);
-}
-.wd-spin-wrap > div:nth-child(11) {
-  -webkit-animation: wd-spin-wrap 1.2s -0s infinite ease-in-out;
-          animation: wd-spin-wrap 1.2s -0s infinite ease-in-out;
-  -webkit-transform: rotate(-60deg);
-          transform: rotate(-60deg);
-}
-.wd-spin-wrap > div:nth-child(12) {
-  -webkit-animation: wd-spin-wrap 1.2s -0s infinite ease-in-out;
-          animation: wd-spin-wrap 1.2s -0s infinite ease-in-out;
-  -webkit-transform: rotate(-30deg);
-          transform: rotate(-30deg);
 }
 .wd-spin-wrap.small > div {
   background-color: #000;
