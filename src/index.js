78b73718a1c7a58310167921f77e214a613059ba
butingting <butingting@weidian.com>
2017-01-04 14:29:18 +0800

    调整InfiniteScroll引入

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import MessageBox from '../packages/MessageBox/index.js'
 import Toast from '../packages/Toast/index.js'
 import InfiniteScroll from '../packages/InfiniteScroll/index.js'
-const install = function() {
+const install = function(Vue) {
   if(install.installed) {
     return
   }
+  Vue.directive('InfiniteScroll', InfiniteScroll)
 }
 
 if (typeof window !== 'undefined' && window.Vue) {
