e43c6b9aa53d684c5c2cf3ca9a8cd7272536da0f
buting <butingting@weidian.com>
2017-01-13 18:40:57 +0800

    增加回调和默认属性设置

--- packages/Search/src/Search.vue
+++ packages/Search/src/Search.vue
@@ -7,26 +7,39 @@
       <div class="wd-search-icon">
         <i class="wd-search-icon-search"></i>
       </div>
-      <input type="text" name="" value="" v-model="inputText" @focus="closeIcon = true" placeholder="请输入搜索内容">
+      <input type="text" name="" value="" v-model="inputText" @focus="closeIcon = true" :placeholder="placeholder">
       <i class="wd-search-input-close" @click="cleanInput" v-show="closeIcon">×</i>
     </div>
-    <div class="wd-search-button">搜索</div>
+    <div class="wd-search-button" @click="search">搜索</div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'wd-search',
+  props: {
+    placeholder: {
+      type: String,
+      default: '请输入搜索内容'
+    },
+    defaultValue: {
+      type: String,
+      default: ''
+    }
+  },
   data() {
     return {
       closeIcon: false,
-      inputText: ''
+      inputText: this.defaultValue
     }
   },
   methods: {
     cleanInput() {
       this.inputText = ''
       this.closeIcon = false
+    },
+    search() {
+      this.$emit('callback', this.inputText)
     }
   }
 }
