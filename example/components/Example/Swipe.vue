efd7eb273f8862e54be017c9d27867cb58b242b2
llissery <llissery@gmail.com>
2017-03-07 14:51:42 +0800

    [doc] update swipe example

--- example/components/Example/Swipe.vue
+++ example/components/Example/Swipe.vue
@@ -1,11 +1,33 @@
 <template>
   <div class="component-wrapper">
-    <wd-swipe class="swipe" :auto="4000" :show-indicators="true">
-      <wd-swipe-item>page1</wd-swipe-item>
-      <wd-swipe-item>page2</wd-swipe-item>
-      <wd-swipe-item>page3</wd-swipe-item>
-      <wd-swipe-item>page4</wd-swipe-item>
-    </wd-swipe>
+    <div class="page-title">Swipe</div>
+    <h2 class="swipe-title">基础用法</h2>
+    <div class="swipe-wrapper">
+      <wd-swipe class="swipe" :auto="4000" :show-indicators="true">
+        <wd-swipe-item>page1</wd-swipe-item>
+        <wd-swipe-item>page2</wd-swipe-item>
+        <wd-swipe-item>page3</wd-swipe-item>
+        <wd-swipe-item>page4</wd-swipe-item>
+      </wd-swipe>
+    </div>
+    <h2 class="swipe-title">取消自动播放</h2>
+    <div class="swipe-wrapper">
+      <wd-swipe class="swipe" :auto="0" :show-indicators="true">
+        <wd-swipe-item>page1</wd-swipe-item>
+        <wd-swipe-item>page2</wd-swipe-item>
+        <wd-swipe-item>page3</wd-swipe-item>
+        <wd-swipe-item>page4</wd-swipe-item>
+      </wd-swipe>
+    </div>
+    <h2 class="swipe-title">隐藏 indicators</h2>
+    <div class="swipe-wrapper">
+      <wd-swipe class="swipe" :auto="4000" :show-indicators="false">
+        <wd-swipe-item>page1</wd-swipe-item>
+        <wd-swipe-item>page2</wd-swipe-item>
+        <wd-swipe-item>page3</wd-swipe-item>
+        <wd-swipe-item>page4</wd-swipe-item>
+      </wd-swipe>
+    </div>
   </div>
 </template>
 
@@ -15,6 +37,18 @@
 </script>
 
 <style lang="css">
+  .component-wrapper .page-title {
+    margin-bottom: 0;
+  }
+  .swipe-wrapper {
+    height: 400px;
+    margin-bottom: 30px;
+  }
+  .swipe-title {
+    margin-top: 15px;
+    text-align: center;
+    font-weight: bold;
+  }
   .component-wrapper {
     height: 400px;
     /*height: 100%;*/
