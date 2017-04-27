bd8842f6f123e9584ce7b3de5c7740782988fc51
jin5354 <xiaoyanjinx@gmail.com>
2017-04-27 16:22:22 +0800

    [refact] integration seperated style file

--- packages/Switch/src/Switch.vue
+++ packages/Switch/src/Switch.vue
@@ -25,5 +25,54 @@
   }
 </script>
 
-<style src="./Switch.scss">
+<style>
+.wd-switch {
+  display: flex;
+  align-items: center;
+  position: relative;
+}
+
+.wd-switch-input {
+  height: 0;
+  width: 0;
+  visibility: hidden;
+}
+
+.wd-switch-button {
+  display: inline-block;
+  position: relative;
+  width: 102px;
+  height: 62px;
+  border: 1px solid #D9D9D9;
+  border-radius: 31px;
+  box-sizing: border-box;
+  background: #D9D9D9;
+}
+.wd-switch-button:after {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 56px;
+  height: 56px;
+  border-radius: 56px;
+  background-color: #FFFFFF;
+  box-shadow: 0 1px 3px rgba(0,0,0,.4);
+  transform: translateX(0);
+  transition:transform .3s;
+}
+
+.wd-switch-input:checked + .wd-switch-button {
+  background: #09BB07;
+  display: inline-block;
+  position: relative;
+  border: 1px solid grey;
+  border-radius: 31px;
+}
+.wd-switch-input:checked + .wd-switch-button:after {
+  transform: translateX(41px);
+}
+.wd-switch-button:active:after {
+  /*width: 60px;*/
+}
 </style>
