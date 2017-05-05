1de293ead7fbf65d4ea7da48fc1ac8d8c390c065
jin5354 <xiaoyanjinx@gmail.com>
2017-05-05 15:23:49 +0800

    [fix] fix Switch component style

--- packages/Switch/src/Switch.vue
+++ packages/Switch/src/Switch.vue
@@ -39,23 +39,22 @@
 .wd-switch-button {
   display: inline-block;
   position: relative;
-  width: 102px;
-  height: 62px;
-  border: 1px solid #D9D9D9; /* no */
+  width: 100px;
+  height: 60px;
+  border: 1px solid #D9D9D9;
   border-radius: 31px;
-  box-sizing: border-box;
   background: #D9D9D9;
 }
 .wd-switch-button:after {
   content: "";
   position: absolute;
-  top: 2px; /* no */
+  top: 2px;
   left: 0;
   width: 56px;
   height: 56px;
   border-radius: 56px;
   background-color: #FFFFFF;
-  box-shadow: 0 1px 3px rgba(0,0,0,.4); /* no */
+  box-shadow: 0 1px 3px rgba(0,0,0,.4);
   transform: translateX(0);
   transition:transform .3s;
 }
@@ -64,10 +63,10 @@
   background: #09BB07;
   display: inline-block;
   position: relative;
-  border: 1px solid grey; /* no */
+  border: 1px solid grey;
   border-radius: 31px;
 }
 .wd-switch-input:checked + .wd-switch-button:after {
-  transform: translateX(41px);
+  transform: translateX(43px);
 }
 </style>
