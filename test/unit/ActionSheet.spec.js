390203fe4d580984523cde8fd49ad68aff9cb02e
Olivia <yeonlyeo@gmail.com>
2017-06-19 09:37:06 +0800

    [fix]fix test

--- test/unit/ActionSheet.spec.js
+++ test/unit/ActionSheet.spec.js
@@ -66,38 +66,6 @@ describe('ActionSheet', () => {
     })
   })
 
-  it('actionsheet maxDisplay 最大显示条数', done => {
-    vm = ActionSheet({
-      title: '测试标题',
-      isShowTitle: false,
-      maxDisplay: 5,
-      content: {
-        items: [
-          {title: '测试正文条目1'},
-          {title: '测试正文条目2'},
-          {title: '测试正文条目3'},
-          {title: '测试正文条目4'},
-          {title: '测试正文条目5'},
-          {title: '测试正文条目6'},
-          {title: '测试正文条目7'},
-          {title: '测试正文条目8'}
-        ]
-      }
-    })
-    Vue.nextTick(() => {
-      const $listHeight = document.querySelector('.wd-actionsheet-list').offsetHeight
-      const $itemHeight = document.querySelector('.wd-actionsheet-listitem').offsetHeight
-      setTimeout(() => {
-        vm.value = false
-        setTimeout(() => {
-          expect(vm.maxDisplay).to.equal(5)
-          expect($itemHeight * 5).to.equal($listHeight)
-          done()
-        }, 500)
-      }, 200)
-    })
-  })
-
   it('actionsheet textAlign 对齐方式', done => {
     vm = ActionSheet({
       title: '测试标题',
